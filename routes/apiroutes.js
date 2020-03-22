ae2da2339e6fb9f89e2d5afc7d7ca6658b667538
Gwendolyn Thomas <gwenthomas85@gmail.com>
2020-03-22 09:09:37 -0700

    updates

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,11 +1,14 @@
 const fs = require ("fs");
+const express = require("express")
 let userInput = require("../db/db.json");
 let currentIDS = userInput.map(note=>note.id);
+
+//Get Requests//
 
 module.exports = function (app) {
     app.get("/api/notes", function(req, res) {
         res.json(userInput);
-      });
+      
 
 
 //Post Request//
@@ -28,5 +31,38 @@ app.post("/api/notes", function(req, res){
         if (err) throw err
     });
     res.json(newNote)
+
+
+    app.delete("/api/notes/:id",(req, res)=>{
+        let id = req.param.id
+        console.log(id)
+        newNote.remove(id, function (err){
+            if(err){
+                console.log(err);
+            }
+            res.send("Success");
+        })
+
+    })
+//delete Request//
+});
+
+//    delete  [id]
+//     //let id = req.params.id;
+//     console.log("./db/db.json", JSON.stringify(id))
+//     //     data=JSON.parse(data);
+//     // });
+//     //delete data ["notes" + id];
+
+        
+//       let check = res.send('Got a DELETE request at /api/notes/:id');
+//     console.log(check)
+
+
+    // ("/api/notes", function(req, res) {
+    //     res.json(userInput);
+    //   });
+
 })
+
 }
